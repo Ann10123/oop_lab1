b0002811fd66d22e25cef0715378e314a97e270b
Ann10123 <152207473+Ann10123@users.noreply.github.com>
2024-10-16 16:32:48 +0300

    Add files via upload

---
+++ main_block1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+
+namespace rectangle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            try
+            {
+                Console.Write("Введіть першу сторону: ");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("Введіть другу сторону: ");
+                double b = double.Parse(Console.ReadLine());
+                Rectangle rectangle = new Rectangle(a, b);
+                Console.Write("Периметр прямокутника: ");
+                Console.WriteLine(rectangle.Perimeter);
+                Console.Write("Площа прямокутника: ");
+                Console.WriteLine(rectangle.Area);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ rectangle_block1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+
+namespace rectangle
+{
+    public class Rectangle
+    {
+        private double a, b;
+        public Rectangle(double a, double b)
+        {
+            if (a <= 0 || b <= 0)
+            {
+                throw new ArgumentException("Неправильний формат чисел!");
+            }
+            this.a = a;
+            this.b = b;
+        }
+        public double A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                if (a > 0)
+                {
+                    a = value;
+                }
+            }
+        }
+        public double B
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                if (b > 0)
+                {
+                    b = value;
+                }
+            }
+        }
+        public double Perimeter
+        {
+            get
+            {
+                return 2 * (a + b);
+            }
+        }
+        public double Area
+        {
+            get
+            {
+                return a * b;
+            }
+        }
+    }
+}
